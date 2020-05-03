fc32c96e65adbd3cbb4d4615e38ec7a115f4e4ec
kiminno <nancyngo141@gmail.com>
2020-05-03 10:41:04 -0400

    Merged SignIn and AddList

---
+++ lib/addList.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+Widget addList = Container(
+  padding: EdgeInsets.only(left: 20, right: 20),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      Text(
+        'Enter your list',
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: 'roboto', //'Comfortaa',
+          fontWeight: FontWeight.bold,
+          fontSize: 40.0,
+          color: Colors.black,
+        ),
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+            labelText:
+                'eg. 1 romaine cabbage, 3 bananas, 24 large brown eggs, 12 roll toilet paper'),
+      ),
+      SizedBox(height: 20),
+      Text(
+        '-OR-',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontFamily: 'roboto',
+          fontWeight: FontWeight.w900,
+          fontSize: 13,
+          color: Colors.black,
+        ),
+      ),
+      SizedBox(height: 20),
+      Container(
+        padding: EdgeInsets.only(left: 70, right: 70),
+        child: RaisedButton(
+          onPressed: () {},
+          color: Colors.orange,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'UPLOAD A PICTURE',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'roboto',
+                fontWeight: FontWeight.w900,
+                fontSize: 13,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ],
+  ),
+);
--- lib/innershome.dart
+++ lib/innershome.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'auth.dart';
+import './addList.dart';
 
 class Home extends StatelessWidget {
-
   final Authentication _auth = Authentication();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Colors.green[50],
+      backgroundColor: Colors.green[50],
       // Add Nancys home screen design
       appBar: AppBar(
         actions: <Widget>[
-        FlatButton.icon(
-          icon: Icon(Icons.person),
-          label: Text("logout"),
-          onPressed: () async {
-            await _auth.signOut();
-        },
-        )
-      ],
-      )    
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text("logout"),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: addList,
+      ),
     );
   }
 }
